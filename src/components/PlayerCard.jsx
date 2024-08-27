ab28bdf20ef8c07dd6c7a5ec5febf3091cf0d1bc
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-08-27 19:49:19 +0300

    playercard güncellend,i istatistikler eklendi style yapılacak

--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
+const PlayerCard = ({ players }) => {
+  const [info, setInfo] = useState(Array(players.length).fill(false));
 
-const PlayerCard = ({players}) => {
+  const toggleStats = (index) => {
+    const newInfo = [...info];
+    newInfo[index] = !newInfo[index];
+    setInfo(newInfo);
+  };
   return (
     <div className="row">
       {players.map((item, index) => (
-        <div className="col-xl-3 col-lg-4 col-md-6 g-3" key={index}>
+        <div
+          className="col-xl-3 col-lg-4 col-md-6 g-3"
+          key={index}
+          onClick={() => toggleStats(index)}
+        >
           <div className="card-container">
             <div className="img-div">
               <img src={item.img} alt={item.name} className="player-img" />
             </div>
             <h5>{item.name}</h5>
+            {info[index] && (
+              <div className="statistick">
+                <p>-{item.statistics}</p>
+              </div>
+            )}
           </div>
         </div>
       ))}
