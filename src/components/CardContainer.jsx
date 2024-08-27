beeb21275f664355a2722e0dbe4d4660d62f0fa4
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-08-27 11:36:06 +0300

    bg yeniden eklendi

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import bgimg from "../assets/nba-bg.jpg";
 import PlayerCard from "./PlayerCard";
 
 const CardContainer = () => {
   return (
     <div className="container nba">
-      <div className="img-div">
-        <img src={bgimg} alt="nba" className="nba-bg" />
+      <div>
         <PlayerCard />
       </div>
     </div>
